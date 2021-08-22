1fffea34ef22b793dfc9971dddc561cfd431709a
Julio Aguilera Jr <julioaguilera337@gmail.com>
2021-08-21 19:58:10 -0500

    assignment completed

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class TicTacToe
 
     def initialize
@@ -27,22 +29,94 @@ class TicTacToe
         input.to_i - 1
     end
 
-    def move(@board, index)
-        board[index] = value
+    def move(index, value = X)
+        @board[index] = value
     end
 
+    def position_taken?(index)
+        @board[index] == "X" || @board[index] == "O"
+    end
 
+    def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+    end
 
+    def turn_count
+        count = 0
+        @board.each do |index|
+            #binding.pry
+            if index == "O" || index == "X"
+                count += 1
+            end
+        end
+        count
+    end
 
+    def current_player
+        if turn_count % 2 == 0
+            "X"
+        else
+            "O"
+        end
+    end
 
+    def turn
+        puts "Please enter a number between 1-9:"
+        input = gets.chomp()
+        index = input_to_index(input)
+        if valid_move?(index)
+          move(index, current_player)
+          display_board
+        else
+          turn
+        end
+    end
+    
+    def won?
+        WIN_COMBINATIONS.detect do |win|
+            #binding.pry
+            if @board[win[0]] == "X" && @board[win[1]] == "X" && @board[win[2]] == "X"
+                return win
+            elsif
+                @board[win[0]] == "O" && @board[win[1]] == "O" && @board[win[2]] == "O"
+                return win
+            end
+        end
+        false
+     end
 
+    def full?
+        @board.each do |spot|
+            if spot == " " 
+                return false
+            end
+        end
+    end
 
+    def draw?
+        full? && !won?
+    end
 
+    def over?
+        won? || draw?
+    end
 
+    def winner
+        if won?
+            @board[won?[0]]
+        else
+            return nil
+        end
+    end
 
-
-
-
-
-
+    def play
+        turn until over?
+        if won?
+            puts "Congratulations #{winner}!"
+        elsif
+            draw?
+            puts "Cat's Game!"
+        end
+    end
+    
 end
